2aabc7dff6f32e3dcc167f4536da891cda8d8ec2
koksal223 <i.salihkoksal@gmail.com>
2021-09-13 08:34:54 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication40/ConsoleApplication40.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+class mal {
+	string isim;
+	float fiyat;
+	int adet;
+public:
+	static float tutar;
+	mal(){}
+	mal(string i, float f, int a):isim(i),fiyat(f),adet(a)  {
+	tutar += (adet * fiyat);
+	}
+	
+
+};
+float mal::tutar = 0;
+int main()
+{
+	int adet;
+	float f;
+	string ad;
+	int sayi;
+	cout << "kac urun girilecegini giriniz : ";
+	cin >> sayi;
+	mal *a=new mal[sayi];
+	for (int i = 0; i < sayi; i++)
+	{
+
+		cout << "ismini girin ";
+		cin >> ad;
+		cout << "fiyati gir : ";
+		cin >> f;
+		cout << "adeti girin ";
+		cin >> adet;
+		a[i] = { ad, f, adet };
+		}
+	cout << "toplam tutar : "<<a[1].tutar;
+
+
+}
+
+
+
